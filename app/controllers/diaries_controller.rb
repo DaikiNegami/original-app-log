71cf7708332c6ee33a0040ec4efb5ea38e541b5d
DaikiNegami <d_0615.baseball@icloud.com>
2020-10-03 13:29:12 +0900

    コントローラー編集

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -4,4 +4,23 @@ class DiariesController < ApplicationController
     @diaries = Diary.all
   end
   
+  def new
+    @diary = Diary.new
+  end
+
+  def create
+    @diary = Diary.new(diary_params)
+    if @diary.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def diary_params
+    params.require(:diary).permit(:image, :title, :text,).merge(user_id: current_user.id)
+  end
+
 end
